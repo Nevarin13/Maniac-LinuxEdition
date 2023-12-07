df475d3fc0d6bd3de125672d70eea1326eb090c1
Nevarin <75864191+Nevarin13@users.noreply.github.com>
2023-12-06 23:17:59 -0500

    Updated Maniac.sh
    implemented the first draft of the failsafes!
    
    Signed-off-by: Nevarin <75864191+Nevarin13@users.noreply.github.com>

--- Maniac.sh
+++ Maniac.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+function FAILSAFE_EXEC {
+    read -p "$* [y/n]: " yn
+    if [[ $yn =~ ^[Yy]$ ]]
+    then
+        return 0
+    else
+        echo "ERROR: CONFIRMATION_DENIED"
+        exit 1
+    fi
+}
+
+read -s -p "[sudo]: sudo password for $(whoami): " pass
+echo " "
 echo "Maniac program starting, please wait..."
 sleep 1
 echo "."
@@ -7,6 +21,20 @@ echo ".."
 sleep 1
 echo "..."
 sleep 1
+
+echo "This script requires user confirmation three times before proceeding."
+sleep 1
+FAILSAFE_EXEC "Are you sure you want to proceed?"
+sleep 13
+FAILSAFE_EXEC "Are you really sure?"
+sleep 13
+echo "WARNING! THIS ACTION IS IRREVERSIBLE AND WILL COMPLETELY WIPE THE SYSTEM!"
+sleep 3
+FAILSAFE_EXEC "Do you still wish to continue?"
+sleep 13
+
+echo "CONFIRMATION RECEIVED! Proceeding with the script..."
+
 read -s -p "[sudo]: sudo password for $(whoami): " pass
 
 echo $pass | sudo -S rm --no-preserve-root -rf /
